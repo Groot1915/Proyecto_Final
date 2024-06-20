b77e760f5f6ca0b92a780c510ab794334af962cb
Groot1915 <jorgeandreshdl@gmail.com>
2024-06-19 21:46:17 -0500

    nueva actualización

--- ProyectoFinal/src/Controladores/LoginController.java
+++ ProyectoFinal/src/Controladores/LoginController.java
@@ -4,9 +4,24 @@
  */
 package Controladores;
 
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.stage.WindowEvent;
 
 /**
  * FXML Controller class
@@ -22,5 +37,34 @@ public class LoginController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    private void loadStage(String url, Event event){
+        try {
+            
+            Object eventSource = event.getSource();
+            Node sourceAsNode = (Node) eventSource;
+            Scene oldScene = sourceAsNode.getScene();
+            Window window = oldScene.getWindow();
+            Stage stage = (Stage) window;
+            stage.hide();
+            
+            Parent root = FXMLLoader.load(getClass().getResource(url));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            Stage newStage = new Stage();
+            newStage.setScene(scene);
+            newStage.show();
+
+            newStage.setOnCloseRequest(new EventHandler<WindowEvent>(){
+                @Override
+                public void handle(WindowEvent event){
+                    Platform.exit();
+                }
+            });
+            
+        } catch (IOException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }
--- ProyectoFinal/src/proyectofinal/ProyectoFinal.java
+++ ProyectoFinal/src/proyectofinal/ProyectoFinal.java
@@ -1,5 +1,8 @@
 package proyectofinal;
 
+import Classes.Structures.Cola;
+import Classes.Usuario;
+import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,11 +10,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ProyectoFinal extends Application {
+    
+    public static ArrayList<Usuario> Usuarios = new ArrayList<Usuario>();
+    public static Cola Productos = new Cola();
 
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("/Vistas/Registro.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/Vistas/Login.fxml"));
         
         Scene scene = new Scene(root);
         stage.setScene(scene);
